176eacb7a091db8919c12ee6fcc446b71afb089f
wangyu <18397642848@163.com>
2017-03-27 23:14:01 +0800

    -v1.0.1

---
+++ robotv1.0/js/class.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 29043 on 2017/3/27.
+ */
+$("#cj-btn").click(function(){
+  $(this).parent().siblings(".caret").css("left","65px");
+})
+$("#zj-btn").click(function(){
+  $(this).parent().siblings(".caret").css("left","180px");
+})
+$("#gj-btn").click(function(){
+  $(this).parent().siblings(".caret").css("left","290px");
+})
+$("#js-btn").click(function(){
+  $(this).parent().siblings(".caret").css("left","400px");
+})
+$("#mf-btn").click(function(){
+  $(this).parent().siblings(".caret").css("left","520px");
+})
