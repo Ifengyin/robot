10f6b54920d8147c11729b9df242f45f2b473baa
Your Name <email@example.com>
2017-03-31 17:39:04 +0800

    robot-v1.0.0.1

--- robotv1.0/js/class.js
+++ robotv1.0/js/class.js
@@ -16,3 +16,55 @@ $("#js-btn").click(function(){
 $("#mf-btn").click(function(){
   $(this).parent().siblings(".caret").css("left","520px");
 })
+
+var classData={"class":[{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+  {"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+  {"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"},
+	{"name":"单向旋转门","teacher":"陈日恒","type":"初级","url":"http://www.baidu.com","details":"简单的机械动态控制，利用旋转的原理，向学生讲述旋转世界的奥秘，培养学生的思考逻辑能力，培养机器人学习的认知...","learnpepple":"1021"}
+]};
+var colorArray=[
+	{start:"rgba(255,150,0,.7)",end:"#ff9600"},{start:"rgba(255,115,155,.7)",end:"#ff739b"},{start:"rgba(210,115,230,.7)",end:"#d273e6"},
+	{start:"rgba(0,185,90,.7)",end:"#00b95a"},{start:"rgba(0,200,200,.7)",end:"#00c8c8"},{start:"rgba(75,150,220,.7)",end:"#4b96dc"}
+];
+
+  $(function(){
+  		var html="";
+      $.each(classData.class,function(index,obj){
+        var jpgCount=Math.floor(Math.random()*20+1);
+        var colorCount=Math.floor(Math.random()*5);
+				var bgcolor=colorArray[colorCount];
+        var html1=`<div class="class-group">
+        <div class="class-detail-title" style="background-image: linear-gradient(270deg,${bgcolor.start},${bgcolor.end});">
+          <img src="../images/class-title-bg/cover${jpgCount}.jpg" class="bg-cover" alt="">
+          <span>
+            <a href="#">${obj.name}</a>
+          </span>
+        </div>
+        <div class="class-detail">
+          <h5><a href="#">${obj.name}--${obj.teacher}</a></h5>
+          <div class="class-detail-ds">
+            ${obj.details}
+          </div>
+          <span class="class-detail-tj">${obj.type}.${obj.learnpepple}人在学习</span>
+        </div>
+      </div>`;
+        html+=html1;
+      })
+			$(".container-body").html(html);
+  })
